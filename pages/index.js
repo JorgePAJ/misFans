4f3f5a2e028b64f53bbb888e6fa68bd695af3e12
Jorge Plasencia <66725751+WaberHoruhe@users.noreply.github.com>
2022-02-23 19:37:50 -0600

    added variables to some placeholders

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,17 @@
 import imagePlace from "../public/placeholder.png";
 import Image from "next/image";
+import React, { useEffect, useState } from "react";
+
 export default function Home() {
+  const username = "Jorge Plasencia";
+  const [nombre, setNombre] = useState("Default");
+  const [cantidad, setCantidad] = useState(0);
+  const [sonido, setSonido] = useState("Sound 1");
   return (
     // Main div
     <div>
       {/* // Username div */}
-      <p>Username</p>
+      <p>{username}</p>
       <Image src={imagePlace} width="100%" height="100%"></Image>
       <div></div>
       {/* // Elements div */}
@@ -16,14 +22,38 @@ export default function Home() {
           <div>
             <p>$</p>
             {/* cantidad seleccionada VARIABLE */}
-            <h2>{}</h2>
+            <h2>{cantidad}</h2>
           </div>
           {/* // Botones div */}
           <div>
-            <button>$10.00</button>
-            <button>$15.00</button>
-            <button>$20.00</button>
-            <button>$25.00</button>
+            <button
+              onClick={() => {
+                setCantidad(10);
+              }}
+            >
+              $10.00
+            </button>
+            <button
+              onClick={() => {
+                setCantidad(15);
+              }}
+            >
+              $15.00
+            </button>
+            <button
+              onClick={() => {
+                setCantidad(20);
+              }}
+            >
+              $20.00
+            </button>
+            <button
+              onClick={() => {
+                setCantidad(25);
+              }}
+            >
+              $25.00
+            </button>
           </div>
         </div>
         {/* //Info div */}
@@ -46,16 +76,43 @@ export default function Home() {
             <h5>Selecciona un sonido</h5>
             {/* // botones sonidos div */}
             <div>
-            <button>Sound 1</button>
-            <button>Sound 2</button>
-            <button>Sound 3</button>
-            <button>Sound 4</button>
+              <button
+                onClick={() => {
+                  setSonido("Sound 1");
+                }}
+              >
+                Sound 1
+              </button>
+              <button
+                onClick={() => {
+                  setSonido("Sound 2");
+                }}
+              >
+                Sound 2
+              </button>
+              <button
+                onClick={() => {
+                  setSonido("Sound 3");
+                }}
+              >
+                Sound 3
+              </button>
+              <button
+                onClick={() => {
+                  setSonido("Sound 4");
+                }}
+              >
+                Sound 4
+              </button>
             </div>
           </div>
           {/* // Pay div */}
           <div>
-            <button>Pagar cantidad  mxn</button>
-            <p>Al continuar, aceptas nuestros términos de servicio y póliticas de privacidad.</p>
+            <button>Pagar {cantidad} mxn</button>
+            <p>
+              Al continuar, aceptas nuestros términos de servicio y póliticas de
+              privacidad.
+            </p>
           </div>
         </div>
       </div>
